14898c8e5114afbf7d03fc9a31ac64f04459539c
Levin Keller <post@levinkeller.de>
2018-03-08 12:43:30 +0100

    Further renaming of auth0 to general JWKS Clean up a bit Fix unconfigurable hostname in certificate.

--- example/__tests__/authentication.ts
+++ example/__tests__/authentication.ts
@@ -1,15 +1,14 @@
-import createAuth0Mock from '../../index'
+import createJWKSMock from '../../index'
 import createApp from '../api'
 import * as supertest from 'supertest'
-import * as superagent from 'superagent'
 
 let server
 let request
-let auth0Mock
+let jwksMock
 
 describe('Some tests for authentication for our api', () => {
   beforeEach(async () => {
-    auth0Mock = createAuth0Mock('https://hardfork.eu.auth0.com')
+    jwksMock = createJWKSMock('https://hardfork.eu.auth0.com')
     server = await createApp({
       jwksHost: 'https://hardfork.eu.auth0.com'
     }).listen()
@@ -17,22 +16,22 @@ describe('Some tests for authentication for our api', () => {
   })
   afterEach(async () => {
     await server.close()
-    await auth0Mock.stop()
+    await jwksMock.stop()
   })
   it('should not get acces without correct token', async () => {
-    auth0Mock.start()
+    jwksMock.start()
     await request.get('/').expect(401)
   })
   it('should get access with mock token when auth0Mock is running', async () => {
-    auth0Mock.start()
-    const access_token = auth0Mock.token({
+    jwksMock.start()
+    const access_token = jwksMock.token({
       aud: 'private',
       iss: 'master'
     })
     await request.get('/').set('Authorization' , `Bearer ${access_token}`).expect(200)
   })
   it('should not get access with mock token when auth0Mock is not running', async () => {
-    const access_token = auth0Mock.token({
+    const access_token = jwksMock.token({
       aud: 'private',
       iss: 'master'
     })
