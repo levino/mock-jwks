c9261a5dcd08f26a6a089a9eb2dce8172d096709
Levin Keller <post@levinkeller.de>
2025-02-05 11:10:06 +0000

    allow nock@14 as a peer dep fixes #236

--- jestSetup.js
+++ jestSetup.js
@@ -2,9 +2,10 @@ jest.mock('nock', () => {
   const versions = {
     "11": "nock-11",
     "12": "nock-12",
-    "13": "nock"
+    "13": "nock-13",
+    "14": "nock",
   }
-  const nockVersion = process.env.NOCK_VERSION || "11";
+  const nockVersion = process.env.NOCK_VERSION || "14";
   const nock = jest.requireActual(versions[nockVersion]);
   return nock
 })
