f8f772718cf05f6e903c6b4d7f0a3fe0373d01a8
Levin Keller <post@levinkeller.de>
2025-03-24 08:42:24 +0000

    simplify test to reproduce #253

--- src/startStop.test.ts
+++ src/startStop.test.ts
@@ -1,31 +1,10 @@
-import jwksClient from 'jwks-rsa'
 import { beforeEach, describe, expect, test } from 'vitest'
 import { createJWKSMock } from './index.js'
-
 const auth0Mock = createJWKSMock('https://hardfork.eu.auth0.com')
-const client = jwksClient({
-  jwksUri: 'https://hardfork.eu.auth0.com/.well-known/jwks.json',
-})
 
-describe('Can start and stop', () => {
-  test('Start, stop and start again', () => {
-    let stop = auth0Mock.start()
-    expect(() => stop()).not.toThrow()
-    stop = auth0Mock.start()
-    expect(() => stop()).not.toThrow()
-  })
-  test('Start twice', () => {
-    expect(auth0Mock.start).not.toThrow()
-    expect(auth0Mock.start).not.toThrow()
-  })
-  describe('in the before hook', () => {
-    beforeEach(() => {
-      auth0Mock.start()
-      auth0Mock.start()
-      auth0Mock.start()
-    })
-    test('Can stop', () => {
-      expect(() => auth0Mock.stop()).not.toThrow()
-    })
-  })
+test('Can start twice, then stop, then start again', () => {
+  auth0Mock.start()
+  auth0Mock.start()
+  auth0Mock.stop()
+  expect(auth0Mock.start).not.toThrow()
 })
